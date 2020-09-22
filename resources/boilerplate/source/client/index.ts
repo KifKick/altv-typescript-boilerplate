3de66a92582fe28e0323199b1b381d749da5d639
KifKick <kifkick@gmail.com>
2020-09-22 21:04:33 +0200

    woops

--- resources/boilerplate/source/client/index.ts
+++ resources/boilerplate/source/client/index.ts
@@ -10,28 +10,6 @@ import exampleModule from 'client/modules/exampleModule/init'
 
 const init = async() => {
 	await exampleModule()
-
-
-	alt.on('test', (testarg: number, bleble: string) => {
-		alt.log(testarg)
-	})
-
-	alt.on('disconnect', () => {
-		alt.log('disconnected')
-	})
-
-	alt.on('gameEntityCreate', (entity) => {
-		alt.log(typeof entity)
-	})
-
-	alt.on('gameEntityDestroy', (entity) => {
-		alt.log(typeof entity)
-	})
-
-	alt.on('syncedMetaChange', (myarg: number, arg) => {
-		alt.log(typeof myarg)
-	})
-
 }
 
 init()
